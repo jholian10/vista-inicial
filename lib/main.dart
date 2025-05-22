c437b2e165a14f716a8359bac94757e85dc444fd
jholian1010 <jholianmanuel10@gmail.com>
2025-05-21 19:45:15 -0500

    codificar mi primera view

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mi_app/features/home/presentation/bloc/home_bloc.dart';
-
 import 'features/home/presentation/views/failure_view.dart';
 import 'features/home/presentation/views/initial_view.dart';
 import 'features/home/presentation/views/loading_view.dart';
